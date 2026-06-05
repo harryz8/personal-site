3a2d011226a00731b6916ab7daef5f6a25f6e362
harryz8 <harry.zoeftig@btinternet.com>
2026-06-05 16:58:12 +0100

    added sidebar component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './home/home';
+import { Sidebar } from "./sidebar/sidebar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home, Sidebar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
